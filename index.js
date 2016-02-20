cd46412c59be9da43cd495574e1fe8874e5bd63d
Шухрат Шадмонкулов <zumra6a@yandex-team.ru>
2016-02-21 02:42:47 +0300

    savepoint

--- index.js
+++ index.js
@@ -5,40 +5,50 @@ var getContentsOf = require('./extractor');
 var newPackage = '/Users/zumra6a/Downloads/tmp/releases/report-templates-r331-join-tc-2654603-rc.tar';
 var oldPackage = '/Users/zumra6a/Downloads/tmp/releases/report-templates-r330-join-tc-2648560-rc.tar';
 
+var packageNewFilesInfo = function(newPackageFiles, oldPackageFiles) {
+    var files = _.difference(newPackageFiles, oldPackageFiles);
+    if (!_.isEmpty(files)) {
+        console.info('Some files were added to the package!');
+        console.log(files.join('\n'));
+    }
+};
+
+var packageRemovedFilesInfo = function(newPackageFiles, oldPackageFiles) {
+    var files = _.difference(oldPackageFiles, newPackageFiles);
+    if (!_.isEmpty(files)) {
+        console.info('Some files were removed from the package!');
+        console.log(files.join('\n'));
+    }
+};
+
+var calculateChangeRateFiles = function(files, newPackageContents, oldPackageContents) {
+    var oldSize = 0;
+    var newSize = 0;
+
+    _.each(files, function(filePath) {
+        oldSize += oldPackageContents[filePath];
+        newSize += newPackageContents[filePath];
+    });
+
+    if ( oldSize * 1.02 < newSize) {
+        console.error('qweqweqweqwe');
+    }
+
+    console.log('old size', oldSize);
+    console.log('new size', newSize);
+};
+
 vow.all([getContentsOf(newPackage), getContentsOf(oldPackage)])
     .spread(function(newPackageContents, oldPackageContents) {
         var newPackageFiles = _.keys(newPackageContents);
         var oldPackageFiles = _.keys(oldPackageContents);
 
-        var addedFiles = _.difference(newPackageFiles, oldPackageFiles);
-        if (!_.isEmpty(addedFiles)) {
-            console.info('Some files were added to the package!');
-            console.log(addedFiles.join('\n'));
-        }
-
-        var removedFiles = _.difference(oldPackageFiles, newPackageFiles);
-        if (!_.isEmpty(removedFiles)) {
-            console.info('Some files were removed from the package!');
-            console.log(removedFiles.join('\n'));
-        }
+        packageNewFilesInfo(newPackageFiles, oldPackageFiles);
+        packageRemovedFilesInfo(newPackageFiles, oldPackageFiles);
 
         var updatedFiles = _.intersection(newPackageFiles, oldPackageFiles);
-        if (!_.isEmpty(updatedFiles)) {
-            var oldSize = 0;
-            var newSize = 0;
+        calculateChangeRateFiles(updatedFiles, newPackageContents, oldPackageContents);
 
-            _.each(updatedFiles, function(filePath) {
-                oldSize += oldPackageContents[filePath];
-                newSize += newPackageContents[filePath];
-            });
-
-            if ( oldSize * 1.02 < newSize) {
-                console.error('qweqweqweqwe');
-            }
-
-            console.log('old size', oldSize);
-            console.log('new size', newSize);
-        }
     })
     .fail(console.error);
 
